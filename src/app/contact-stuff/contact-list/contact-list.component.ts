36f27dc31b361cf0f8ce0f6304410fd28f3307c7
Gabriela Parra <parragabriela@icloud.com>
2017-07-14 16:27:18 -0400

    users can see a list of contacts

--- src/app/contact-stuff/contact-list/contact-list.component.ts
+++ src/app/contact-stuff/contact-list/contact-list.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-
+  allTheContacts: any[] = [
+    {
+      id: 42,
+      name: 'Andromeda',
+      notes: 'Weird name from Refresh Miami'
+    },
+    {
+      id: 100,
+      name: 'Arcann',
+      notes: 'Star Wars'
+    },
+    
+  ]
   constructor() { }
 
   ngOnInit() {
